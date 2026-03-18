2813f796127e2699b9f1b7c9e046a50831dd9b1e
lanfeng78 <1584709889@qq.com>
2026-03-18 11:57:13 +0800

    version 1.0

---
+++ src/main/java/com/yupi/yupiaicodemother/controller/HealthController.java
@@ -0,0 +1,8 @@
+package com.yupi.yupiaicodemother.controller;
+
+/**
+ * @author Lanfeng
+ * @version 1.0
+ */
+public class HealthController {
+}
---
+++ src/test/java/com/yupi/yupiaicodemother/YupiAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.yupiaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YupiAiCodeMotherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
